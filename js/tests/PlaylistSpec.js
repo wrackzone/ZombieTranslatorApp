18fdbc7a7d2002e16f7502b6ac97ef82200841d1
JaZahn <jcleveng@fas.harvard.edu>
2015-09-14 00:44:58 -0400

    adding a working specRunner and version of jasmine

---
+++ js/tests/PlaylistSpec.js
@@ -0,0 +1,12 @@
+define(['Playlist'], function(Playlist){
+  var playlist = new Playlist();
+  describe('Playlist', function(){
+    describe('addSong', function(){
+      it('should add a song to the playlist', function(){
+        
+      });
+    });
+  });
+
+
+});
